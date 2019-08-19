df8b4a69163fb7a948292ae1f5e38c780613789f
luhaojian1 <962671114@qq.com>
2019-08-19 17:42:07 +0800

    item quality is 0 when updateQuality given item name is Knife and quality is 0 and sellIn is 20

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -18,5 +18,16 @@ public class GildedRoseTest {
         assertEquals(2, items[0].quality);
     }
 
+    @Test
+    public void should_item_quality_is_0_when_updateQuality_given_item_name_is_Knife_and_quality_is_0_and_sellIn_is_20() {
+
+        Item item = new Item("Knife", 20, 0);
+        Item[] items = {item};
+        GildedRose gildedRose = new GildedRose(items);
+
+        Item[] result = gildedRose.updateQuality();
+
+        assertEquals(0, items[0].quality);
+    }
 
 }
