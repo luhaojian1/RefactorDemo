9466be17971203366a4d7bc5c7f4527a89642535
luhaojian1 <962671114@qq.com>
2019-08-20 08:43:04 +0800

    delete unused static field from GildedRose

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -4,9 +4,6 @@ import java.util.Arrays;
 
 public class GildedRose {
     private Item[] items;
-    private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
 
     public GildedRose(Item[] items) {
         this.items = items;
